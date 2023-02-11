ea77c184e80451acae009227d5cf2dfafb43f54d
Aldwin <13520167@std.stei.itb.ac.id>
2023-02-11 16:33:21 +0700

    feat: add splash screen

---
+++ app/src/main/java/com/majika/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.majika
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val splash = findViewById<ImageView>(R.id.splash_icon)
+
+        splash.alpha = 0f
+        splash.animate().setDuration(1500).alpha(1f).withEndAction{
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
